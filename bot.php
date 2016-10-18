3f875ed8d3a98500d77a04d3b3ebef9efe558b6e
kingrtp86 <kingrtp.86@gmail.com>
2016-10-18 12:59:40 +0700

    add edit line2

--- bot.php
+++ bot.php
@@ -61,7 +61,7 @@ if (!is_null($events['events'])) {
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			$reply_msg = [$msg1, $msg2, $stk];
+			$reply_msg = [$stk];
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$reply_msg]
