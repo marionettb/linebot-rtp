89054276a12a7d6b6bbb465e091d8ee178fe64a9
kingrtp86 <kingrtp.86@gmail.com>
2016-10-18 09:58:41 +0700

    edit

--- bot.php
+++ bot.php
@@ -17,17 +17,38 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-      switch ($text) {
-      	case 'สวัสดี':
-      		echo "สวัสดีครับ";
-      		break;
-      	case 'ทดสอบ':
-					echo "ทดสอบครับ";
-      		break;
-      	default:
-					echo "พิมพ์ไรมา งง";
-      		break;
-      }
+			switch ($text) {
+				case 'สวัสดี':
+					$messages = [
+						'type' => 'text',
+	  				'text' => 'สวัสดีครับ'
+	  			];
+					break;
+				case 'ทดสอบ1':
+					$messages = [
+						'type' => 'text',
+						'text' => 'สวัสดีครับ1'
+					];
+					break;
+				case 'ทดสอบ2':
+					$messages = [
+						'type' => 'text',
+						'text' => 'สวัสดีครับ2'
+					];
+					break;
+				case 'ทดสอบ3':
+					$messages = [
+						'type' => 'text',
+						'text' => 'สวัสดีครับ3'
+					];
+					break;
+				default:
+				$messages = [
+					'type' => 'text',
+					'text' => 'out'
+				];
+					break;
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
