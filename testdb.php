78a9b471351e1c78ab229df68c17de8e8dd99ed6
kingrtp86 <kingrtp.86@gmail.com>
2016-10-19 13:52:21 +0700

    add test text2

--- testdb.php
+++ testdb.php
@@ -1,9 +1,21 @@
 <?php
+  echo "test rtp";
   // $link = mysql_connect('https://linebot.faxthai.com', 'smartschool', 'smartschool456');
   // if (!$link) {
   //   die('connect failed: ' . mysql_error());
   // }
   // echo 'Connect succes';
   // mysql_close($link);
-  echo "test rtp";
+  $servername = "https://linebot.faxthai.com";
+  $username = "smartschool";
+  $password = "smartschool456";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
+
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
 ?>
