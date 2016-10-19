fd96a8b47acbaf558818af0f7a60ee60d3c04252
kingrtp86 <kingrtp.86@gmail.com>
2016-10-19 22:20:48 +0700

    test decode name json6

--- bot2.php
+++ bot2.php
@@ -53,8 +53,8 @@ $text=str_replace("\n", NULL, $text);
 
 //init user Data
 $a=$bot->getProfile($to);
-$profile = decode_json($response->getBody);
-$name = $profile->{'displayName'};
+$profile = json_decode($a);
+$name = $profile->displayName;
 $user_profle=getuser_profile($a);
 $displayName=$user_profle['displayName'];
 $userId=$user_profle['userId'];
@@ -63,7 +63,7 @@ $statusMessage=$user_profle['statusMessage'];
 
 //bot handle
 if ((strstr($text, 'ทดสอบ') !== false)) {
-  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ทดสอบบบบบ 3 Token and '.$name);
+  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ทดสอบบบบบ 4 Token and '.$name);
   $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 }
  ?>
