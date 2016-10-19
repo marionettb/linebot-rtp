472d4674419a46184f1c875ac7384e3d483c40ed
kingrtp86 <kingrtp.86@gmail.com>
2016-10-19 13:40:13 +0700

    add test text

--- testdb.php
+++ testdb.php
@@ -1,9 +1,9 @@
 <?php
-  $link = mysql_connect('https://linebot.faxthai.com', 'smartschool', 'smartschool456');
-  if (!$link) {
-    die('connect failed: ' . mysql_error());
-  }
-  echo 'Connect succes';
-  mysql_close($link);
+  // $link = mysql_connect('https://linebot.faxthai.com', 'smartschool', 'smartschool456');
+  // if (!$link) {
+  //   die('connect failed: ' . mysql_error());
+  // }
+  // echo 'Connect succes';
+  // mysql_close($link);
   echo "test rtp";
 ?>
