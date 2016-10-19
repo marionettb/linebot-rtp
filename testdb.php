91ef2fe076a473450c88529fe2a378500509cf96
kingrtp86 <kingrtp.86@gmail.com>
2016-10-19 12:47:13 +0700

    edit connect mysql3

--- testdb.php
+++ testdb.php
@@ -1,15 +1,15 @@
 <?php
-  $servername = "smartschool@107.167.89.104";
-  $username = "smartschool";
-  $password = "smartschool456";
-
-  // Create connection
-  $conn = new mysql($servername, $username, $password);
-
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  }
+  // $servername = "smartschool@107.167.89.104";
+  // $username = "smartschool";
+  // $password = "smartschool456";
+  //
+  // // Create connection
+  // $conn = new mysql($servername, $username, $password);
+  //
+  // // Check connection
+  // if ($conn->connect_error) {
+  //     die("Connection failed: " . $conn->connect_error);
+  // }
   echo "Connected successfully";
 
 ?>
