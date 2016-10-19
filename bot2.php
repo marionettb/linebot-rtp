912458d84d1eea3f31d6a6856ff41813562ce373
kingrtp86 <kingrtp.86@gmail.com>
2016-10-19 22:39:18 +0700

    restore test display

--- bot2.php
+++ bot2.php
@@ -12,16 +12,25 @@ function objectToArray($d)
         return $d;
     }
 }
-function getuser_profile($to){
-  $response = $bot->getProfile($to);
-  if ($response->isSucceeded()) {
-      $profile = decode_json($response->getBody);
-      $user_profle_data['displayName'] = 'test';
-      // $user_profle_data['pictureUrl'] = $profile->{'pictureUrl'};
-      // $user_profle_data['statusMessage'] = $profile->{'statusMessage'};
-      return $user_profle_data;
-  }
-  //return $user_profle_data;
+function getuser_profile($a){
+  $b=print_r($a,true);
+  $text_ex = explode('=>', $b) ;
+  $text_ex[2]=str_replace('}', NULL, $text_ex[2]);
+  $text_ex[2]=str_replace('{', NULL, $text_ex[2]);
+  $user_profle=explode(',', $text_ex[2]) ;
+  $a=explode(':', $user_profle[0]) ;
+  $a[1]=str_replace('"', NULL, $a[1]);
+  $user_profle_data['displayName']=$a[1];
+  $a=explode(':', $user_profle[1]) ;
+  $a[1]=str_replace('"', NULL, $a[1]);
+  $user_profle_data['userId']=$a[1];
+  $a=explode(':', $user_profle[2]) ;
+  $a[2]=str_replace('"', NULL, $a[2]);
+  $a[2]=str_replace('//', NULL, $a[2]);
+  $user_profle_data['pictureUrl']='http://'.$a[2];
+  $a=explode(':', $user_profle[3]) ;
+  $user_profle_data['statusMessage']=$a[1];
+  return $user_profle_data;
 }
 
 //init line bot
@@ -43,10 +52,8 @@ $text=str_replace("\r", NULL, $text);
 $text=str_replace("\n", NULL, $text);
 
 //init user Data
-// $a=getUserProfile($to);
-// $profile = json_decode($a);
-// $name = $profile->{'displayName'};
-$user_profle=getuser_profile($to);
+$a=$bot->getProfile($to);
+$user_profle=getuser_profile($a);
 $displayName=$user_profle['displayName'];
 $userId=$user_profle['userId'];
 $pictureUrl=$user_profle['pictureUrl'];
@@ -54,7 +61,7 @@ $statusMessage=$user_profle['statusMessage'];
 
 //bot handle
 if ((strstr($text, 'ทดสอบ') !== false)) {
-  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ทดสอบบบบบ 6 Token and '.$displayName);
+  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ทดสอบบบบบ Token and '.$displayName);
   $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 }
  ?>
