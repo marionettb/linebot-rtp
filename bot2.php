de553f65aa761a2b7157b14abac4aa4ad771465b
kingrtp86 <kingrtp.86@gmail.com>
2016-10-19 22:48:29 +0700

    entry point_

--- bot2.php
+++ bot2.php
@@ -13,26 +13,23 @@ function objectToArray($d)
     }
 }
 function getuser_profile($a){
-  $profile = decode_json($a->getBody);
-  $user_profle_data['displayName'] = 'ทดสอบ';
-
-  // $b=print_r($a,true);
-  // $text_ex = explode('=>', $b) ;
-  // $text_ex[2]=str_replace('}', NULL, $text_ex[2]);
-  // $text_ex[2]=str_replace('{', NULL, $text_ex[2]);
-  // $user_profle=explode(',', $text_ex[2]) ;
-  // $a=explode(':', $user_profle[0]) ;
-  // $a[1]=str_replace('"', NULL, $a[1]);
-  // $user_profle_data['displayName']=$a[1];
-  // $a=explode(':', $user_profle[1]) ;
-  // $a[1]=str_replace('"', NULL, $a[1]);
-  // $user_profle_data['userId']=$a[1];
-  // $a=explode(':', $user_profle[2]) ;
-  // $a[2]=str_replace('"', NULL, $a[2]);
-  // $a[2]=str_replace('//', NULL, $a[2]);
-  // $user_profle_data['pictureUrl']='http://'.$a[2];
-  // $a=explode(':', $user_profle[3]) ;
-  // $user_profle_data['statusMessage']=$a[1];
+  $b=print_r($a,true);
+  $text_ex = explode('=>', $b) ;
+  $text_ex[2]=str_replace('}', NULL, $text_ex[2]);
+  $text_ex[2]=str_replace('{', NULL, $text_ex[2]);
+  $user_profle=explode(',', $text_ex[2]) ;
+  $a=explode(':', $user_profle[0]) ;
+  $a[1]=str_replace('"', NULL, $a[1]);
+  $user_profle_data['displayName']=$a[1];
+  $a=explode(':', $user_profle[1]) ;
+  $a[1]=str_replace('"', NULL, $a[1]);
+  $user_profle_data['userId']=$a[1];
+  $a=explode(':', $user_profle[2]) ;
+  $a[2]=str_replace('"', NULL, $a[2]);
+  $a[2]=str_replace('//', NULL, $a[2]);
+  $user_profle_data['pictureUrl']='http://'.$a[2];
+  $a=explode(':', $user_profle[3]) ;
+  $user_profle_data['statusMessage']=$a[1];
   return $user_profle_data;
 }
 
@@ -58,13 +55,13 @@ $text=str_replace("\n", NULL, $text);
 $a=$bot->getProfile($to);
 $user_profle=getuser_profile($a);
 $displayName=$user_profle['displayName'];
-// $userId=$user_profle['userId'];
-// $pictureUrl=$user_profle['pictureUrl'];
-// $statusMessage=$user_profle['statusMessage'];
+$userId=$user_profle['userId'];
+$pictureUrl=$user_profle['pictureUrl'];
+$statusMessage=$user_profle['statusMessage'];
 
 //bot handle
-if ((strstr($text, 'ทดสอบ') !== false)) {
-  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ทดสอบบบบบ '.$displayName);
+if ((strstr($text, 'สวัสดี') !== false)) {
+  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('สวัสดีครับ คุณ '.$displayName);
   $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 }
  ?>
