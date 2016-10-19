414153be384ef633776d7d50aa6b5a60e498e610
kingrtp86 <kingrtp.86@gmail.com>
2016-10-19 20:58:14 +0700

    add bot 2

---
+++ bot2.php
@@ -0,0 +1,4 @@
+<?php
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('RuHhciFle36XBJXielhR22aO689nyDsFjrzG0mBBDMvlqTsIWxBJgAdBh5LiyedayUBGmHtd0q4bxYJDbmozMr609DXroXmOyKABrJuGzd9iLpbWcKazlbwlMOORJeAxdVcOYSu8yoaAGANJpSUdqQdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'f241bea6d6db5d7778ad06a7fff1d00b']);
+ ?>
