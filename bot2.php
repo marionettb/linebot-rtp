06413c2fbe7439d71fe06b773b2a54d31872fc9b
kingrtp86 <kingrtp.86@gmail.com>
2016-10-19 23:40:59 +0700

    add audio reply

--- bot2.php
+++ bot2.php
@@ -79,4 +79,11 @@ if ((strstr($text, 'รูป') !== false)) {
   $picMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($urlpic, $urlpic);
   $response = $bot->replyMessage($replyToken, $picMessageBuilder);
 }
+
+if ((strstr($text, 'เพลง') !== false)) {
+  $urlaud = "https://linebot.faxthai.com/linebot-school/public/farewellballad.m4a";
+
+  $audioMessageBuilder = new \LINE\LINEBot\MessageBuilder\AudioMessageBuilder($urlaud, 97000);
+  $response = $bot->replyMessage($replyToken, $audioMessageBuilder);
+}
  ?>
