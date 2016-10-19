bde7abc3342f695d4de6e7b1f14b83f132f42440
kingrtp86 <kingrtp.86@gmail.com>
2016-10-19 21:06:21 +0700

    add bot edit

--- bot2.php
+++ bot2.php
@@ -1,4 +1,11 @@
 <?php
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('od+PiQERR00IzMSuVMVb+hSYQk5J8QIbcq7+C/S9z16pNDJwgn24hZgCe1CPW/qJBpAeNhG/iodo0Og51WJ9GlhX3vuexXb5NyFCeAOtXfS/vF4Edo8uffYTWXbqbyOsaD3oe56qK6+1fGfw4E7wVQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'd35578662d01550b89fdadc466fa1641']);
  ?>
