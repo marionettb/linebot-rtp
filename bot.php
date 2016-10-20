f953d64f8f8508735ded7b1aff2c55b1a51f8227
kingrtp86 <kingrtp.86@gmail.com>
2016-10-20 22:57:10 +0700

    edit connect

--- bot.php
+++ bot.php
@@ -4,5 +4,5 @@ require "src/init.php";
 require "src/handle.php";
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('RuHhciFle36XBJXielhR22aO689nyDsFjrzG0mBBDMvlqTsIWxBJgAdBh5LiyedayUBGmHtd0q4bxYJDbmozMr609DXroXmOyKABrJuGzd9iLpbWcKazlbwlMOORJeAxdVcOYSu8yoaAGANJpSUdqQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '63029feed681ec683d321a94e98bfb8d']);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'da108dc294c0253688b872a630e9fd31']);
  ?>
