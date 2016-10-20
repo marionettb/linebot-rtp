56bb2ad7b7f44c155f64bd5e51d326b0d4b88460
kingrtp86 <kingrtp.86@gmail.com>
2016-10-20 16:08:55 +0700

    add wunderground plk_2

--- bot2.php
+++ bot2.php
@@ -97,14 +97,14 @@ if ((strstr($text, 'อากาศ') !== false)) {
   curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
   //.str_replace(' ', '%20', $text_ex[1]).' partials string in wunderground
-  curl_setopt($ch1, CURLOPT_URL, 'http://api.wunderground.com/api/71f7e550467fb82d/forecast/lang:TH/q/Thailand/phitsanulok.json');
+  curl_setopt($ch1, CURLOPT_URL, 'http://api.wunderground.com/api/cf7dc205e34e58be/forecast/lang:TH/q/Thailand/%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3.json');
   $result1 = curl_exec($ch1);
   curl_close($ch1);
 
   $obj = json_decode($result1, true);
   if(isset($obj['forecast']['txt_forecast']['forecastday'][0]['fcttext_metric'])){
       $result_text = $obj['forecast']['txt_forecast']['forecastday'][0]['fcttext_metric'];
-  }else{//ถ้าไม่เจอกับตอบกลับว่าไม่พบข้อมูล
+  }else{
       $result_text = 'ไม่พบข้อมูล';
   }
   $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($result_text);
