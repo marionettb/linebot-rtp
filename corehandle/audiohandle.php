5fa17eec63360f176a1fb5375d8199e2ab3651be
kingrtp86 <kingrtp.86@gmail.com>
2016-10-20 22:52:18 +0700

    editd

--- corehandle/audiohandle.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ((strstr($text, 'เพลง') !== false)) {
-  $urlaud = "https://linebot.faxthai.com/linebot-school/public/farewellballad.m4a";
-
-  $audioMessageBuilder = new \LINE\LINEBot\MessageBuilder\AudioMessageBuilder($urlaud, 97000);
-  $response = $bot->replyMessage($replyToken, $audioMessageBuilder);
-}
- ?>
