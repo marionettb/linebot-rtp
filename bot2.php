600c6c0f95dacc845f12207b3ae399a6f99c93b7
kingrtp86 <kingrtp.86@gmail.com>
2016-10-20 14:04:59 +0700

    p mi patch/

--- bot2.php
+++ bot2.php
@@ -134,10 +134,20 @@ if ((strstr($text, 'เพลง') !== false)) {
 
 if ((strstr($text, 'เกีย') !== false)) {
     $response = $bot->replyMessage($replyToken, new TemplateMessageBuilder(
-                        'Confirm alt text',
+                        'ทดสอบความเกีย',
                         new ConfirmTemplateBuilder('คุณกลัวเมียหรือไม่?', [
                             new MessageTemplateActionBuilder('กลัว', '555555555555 อ่อน!'),
                             new MessageTemplateActionBuilder('ไม่กลัว', 'ใช่ป่าวววว!'),
+                        ])
+                    ));
+}
+
+if ((strstr($text, 'Confirm') !== false)) {
+    $response = $bot->replyMessage($replyToken, new TemplateMessageBuilder(
+                        'Confirm alt text',
+                        new ConfirmTemplateBuilder('Do it?', [
+                            new MessageTemplateActionBuilder('Yes', 'Yes!'),
+                            new MessageTemplateActionBuilder('No', 'No!'),
                         ])
                     ));
 }
