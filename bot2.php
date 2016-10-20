d8678327be83fa5c5c4a8bce52d9b1d317a08baa
kingrtp86 <kingrtp.86@gmail.com>
2016-10-20 15:15:06 +0700

    p mi patch 6

--- bot2.php
+++ bot2.php
@@ -96,7 +96,7 @@ if ((strstr($text, 'อากาศ') !== false)) {
   $ch1 = curl_init();
     curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch1, CURLOPT_URL, 'http://api.wunderground.com/api/78788e2e6907e18d/forecast/lang:TH/q/Thailand/%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3.json');
+    curl_setopt($ch1, CURLOPT_URL, 'http://api.wunderground.com/api/cf7dc205e34e58be/forecast/lang:TH/q/Thailand/%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3.json');
     $result1 = curl_exec($ch1);
     curl_close($ch1);
 
